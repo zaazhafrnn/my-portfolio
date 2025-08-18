ffecebcc4871538119910235ec30adaf6023ddc4
zaazhafrnn <150093563+zaazhafrnn@users.noreply.github.com>
2025-08-18 22:37:29 +0700

    hide browser app

--- src/components/apps/index.tsx
+++ src/components/apps/index.tsx
@@ -1,12 +1,12 @@
 export { default as ATMApp } from "./ATMApp";
-export { default as Browser } from "./Browser";
+// export { default as Browser } from "./Browser";
 export { default as PhotosApp } from "./PhotosApp";
 export { default as ResumeApp } from "./ResumeApp";
 export { default as SafariLauncher } from "./SafariApp/SafariLauncher";
 export { default as SystemInfoApp } from "./SystemInfoApp";
 
 export const APP_COMPONENTS = {
-  browser: () => import("./Browser").then((m) => m.default),
+  // browser: () => import("./Browser").then((m) => m.default),
   photos: () => import("./PhotosApp").then((m) => m.default),
   resume: () => import("./ResumeApp").then((m) => m.default),
   atmProject: () => import("./ATMApp").then((m) => m.default),
